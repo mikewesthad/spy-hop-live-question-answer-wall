02fce68d8e4855159c5ca8aacbc8d31e1ca464d7
Michael Hadley <michaelwesthadley@gmail.com>
2018-07-18 20:30:30 -0500

    Prettier

--- src/components/text-entry.js
+++ src/components/text-entry.js
@@ -75,7 +75,13 @@ export class TextEntry extends Component {
         >
           {charCount} / {maxCharacters}
         </div>
-        <input className="button" style={{fontSize: "1.1rem"}} disabled={!isValid} type="submit" value={submitText} />
+        <input
+          className="button"
+          style={{ fontSize: "1.1rem" }}
+          disabled={!isValid}
+          type="submit"
+          value={submitText}
+        />
       </form>
     );
   }
